383f060006c406d77e109631898d270517bc2b31
waylon-coats <waylon.coats@covcath.org>
2025-10-03 17:17:08 -0400

    changed something

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -4,15 +4,16 @@ using namespace vex;
 brain Brain;
 controller Controller = controller(primary);
 motor LFM = motor(PORT7, ratio6_1, true);
-motor LMM = motor(PORT6, ratio6_1, false);
+motor LMM = motor(PORT6, ratio6_1, true);
 motor LBM = motor(PORT5, ratio6_1, true);
+
 motor RFM = motor(PORT10, ratio6_1, false);
-motor RMM = motor(PORT9, ratio6_1, true);
+motor RMM = motor(PORT9, ratio6_1, false);
 motor RBM = motor(PORT8, ratio6_1, false);
 
 motor intakeMotorRight = motor(PORT4, ratio18_1, true);
-motor intakeMotorLeft = motor(PORT3, ratio18_1, false);
-motor intakeMotorTop = motor(PORT15, ratio18_1, false);
+motor intakeMotorLeft = motor(PORT3, ratio18_1, true);
+motor intakeMotorTop = motor(PORT15, ratio18_1, true);
 
 inertial inertialSensor = inertial(PORT18);
 
