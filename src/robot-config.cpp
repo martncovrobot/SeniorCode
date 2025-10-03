b61e2012248b78a22df0ca8bb419192f5bc02729
waylon-coats <waylon.coats@covcath.org>
2025-10-03 17:07:23 -0400

    configured motors

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -3,16 +3,16 @@ using namespace vex;
 
 brain Brain;
 controller Controller = controller(primary);
-motor LFM = motor(PORT11, ratio6_1, true);
-motor LMM = motor(PORT12, ratio6_1, false);
-motor LBM = motor(PORT13, ratio6_1, true);
-motor RFM = motor(PORT14, ratio6_1, false);
-motor RMM = motor(PORT15, ratio6_1, false);
-motor RBM = motor(PORT16, ratio6_1, true);
+motor LFM = motor(PORT7, ratio6_1, true);
+motor LMM = motor(PORT6, ratio6_1, false);
+motor LBM = motor(PORT5, ratio6_1, true);
+motor RFM = motor(PORT10, ratio6_1, false);
+motor RMM = motor(PORT9, ratio6_1, true);
+motor RBM = motor(PORT8, ratio6_1, false);
 
-motor intakeMotorRight = motor(PORT1, ratio18_1, true);
-motor intakeMotorLeft = motor(PORT2, ratio18_1, false);
-motor intakeMotorTop = motor(PORT4, ratio18_1, false);
+motor intakeMotorRight = motor(PORT4, ratio18_1, true);
+motor intakeMotorLeft = motor(PORT3, ratio18_1, false);
+motor intakeMotorTop = motor(PORT15, ratio18_1, false);
 
 inertial inertialSensor = inertial(PORT18);
 
